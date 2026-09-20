cd68027d16829c03c5aadc7bb25d907532a4b24b
makhenw <makhenw@gmail.com>
2026-09-21 00:20:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // Edelweiss Digital PWA configuration
-// Deployment Web App GAS produksi.
 const EDELWEISS_GAS_URL = "https://script.google.com/macros/s/AKfycbxisBzOBCKzPy_ZYfM0v57ydeHxHVi1SvK0pAw5u_XsJSfcE-mOTTdSsJn32I4wta04/exec";
+
+window.EDELWEISS_GAS_URL = EDELWEISS_GAS_URL;
